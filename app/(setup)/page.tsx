d82d26effc4e08c4fc4a83c0646556f3465723bf
Adoo Darko Jonathan <adoojonathan412@gmail.com>
2025-09-10 19:20:22 +0000

    Build File Upload

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { db } from '@/lib/db'
+import { initialProfile } from '@/lib/initial-profile'
+import { redirect } from 'next/navigation'
+import { InitialModal } from '@/components/modals/initial-modal'
 
 const SetupPage = async () => {
-   return (
-      <div>
+   const profile = await initialProfile()
+   const server = await db.server.findFirst({
+      where: {
+         members: {
+            some: {
+               profileId: profile.id,
+            },
+         },
+      },
+   })
 
-      </div>
-   )
+   if (server) {
+      return redirect(`/servers/${server.id}`)
+   }
+
+   return <InitialModal />
 }
 
 export default SetupPage
